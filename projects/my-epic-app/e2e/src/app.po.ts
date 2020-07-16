6ac7b48d1b192cf473cc88e5a3bd42d155d79286
Thomas <thomas.gyt@gmail.com>
2020-07-16 14:23:12 +0200

    AEE-2 : Add Dependencies + Main Layout
    - Prettier

--- projects/my-epic-app/e2e/src/app.po.ts
+++ projects/my-epic-app/e2e/src/app.po.ts
@@ -6,6 +6,8 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('my-org-root .content span')).getText() as Promise<string>;
+    return element(by.css('my-org-root .content span')).getText() as Promise<
+      string
+    >;
   }
 }
---
+++ projects/my-epic-app/src/app/core/core.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {AppRoutingModule} from '../app-routing.module';
+
+
+
+@NgModule({
+  declarations: [MainLayoutComponent],
+  imports: [
+    // vendor
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    // material
+    MatToolbarModule,
+    MatButtonModule,
+    AppRoutingModule
+  ],
+  exports: [MainLayoutComponent]
+})
+export class CoreModule {}
