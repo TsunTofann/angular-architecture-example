90bc5059edbbfcd1026b844fa519bcf967e134ac
Thomas <thomas.gyt@gmail.com>
2020-07-28 13:05:46 +0200

    AAE-4 : Add Angular Calendar

--- projects/my-epic-app/src/app/features/admin/admin.component.ts
+++ projects/my-epic-app/src/app/features/admin/admin.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarView,
+} from 'angular-calendar';
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours,
+} from 'date-fns';
+import {Subject} from 'rxjs';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3',
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF',
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA',
+  },
+};
 
 @Component({
   selector: 'my-org-admin',
   templateUrl: './admin.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent {
+  view: CalendarView = CalendarView.Month;
 
-  constructor() { }
+  viewDate: Date = new Date();
 
-  ngOnInit(): void {
+  events: CalendarEvent[] = [];
+
+  changeDay(date: Date) {
+    this.viewDate = date;
+    this.view = CalendarView.Day;
   }
-
 }
--- projects/my-epic-app/src/app/features/admin/admin.module.ts
+++ projects/my-epic-app/src/app/features/admin/admin.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import {SharedModule} from '../../shared/shared.module';
+import {CalendarModule, DateAdapter} from 'angular-calendar';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
+import {CalendarHeaderComponent} from './calendar-header.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, CalendarHeaderComponent],
   imports: [
     SharedModule,
-    AdminRoutingModule
-  ]
+    CommonModule,
+    FormsModule,
+    AdminRoutingModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+  ],
+  exports: [CalendarHeaderComponent],
 })
 export class AdminModule { }
